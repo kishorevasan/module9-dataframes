69ae1f1e97e0a570c756fdb3fbe5e26c55bab94e
kishorevasan <viprankishore@gmail.com>
2017-01-19 15:20:00 -0800

    csv dataframe

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,27 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
-
+score.home <- c(40,10,24,31,25,26,20)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
-
+score.away <- c(7,38,3,34,23,6,36)
 
 # Combine your two vectors into a dataframe
-
+scores<- data.frame(score.home,score.away)
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+scores$diff <- score.home - score.away
 
 # Create a new column "won" which is TRUE if the Seahawks won
-
+scores$won <- scores$dif>0
 
 # Create a vector of the opponent names corresponding to the games played
-
+opponent.names <- c("Panthers","Packers","Rams","Cardinals","49ers","Lions","Falcons")
 
 # Assign your dataframe rownames of their opponents
-
+scores$oppnames <- opponent.names
 
 # View your data frame to see how it has changed!
+View(scores)
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,41 +1,60 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+usdata <- data.frame(USPersonalExpenditure,stringsAsFactors = FALSE)
 
 # What are the column names of your dataframe?
+colnames(usdata)
+test <- colnames(usdata)
+substr(test[1],2,5)
 
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
-# What are the row names of your dataframe?
+prevnames <- substr(colnames(usdata),2,5)
 
+colnames(usdata)<- prevnames
+
+colnames(usdata)
+
+# What are the row names of your dataframe?
+rownames(usdata)
 
 # Create a column "category" that is equal to your rownames
-
+usdata$category <- rownames(usdata)
 
 # How much money was spent on personal care in 1940?
-
+usdata$`1940`[usdata$category=="Personal Care"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+usdata$`1960`[usdata$category=="Food and Tobacco"]
 
 # What was the highest expenditure category in 1960?
 # Hint: use the `max()` function to find the largest, then compare that to the column
-
+usdata$category[usdata$`1960`==max(usdata$`1960`)]
 
 # Define a function `DetectHighest` that takes in a year as a parameter, and
 # returns the highest spending category of that year
-
+DetectHighest<- function(year){
+  print(usdata)
+  test<- paste0("`",year,"`")
+  print(usdata$test)
+  return(usdata$category[usdata$year==max(usdata$year)])
+}
 
 # Using your function, determine the highest spending category of each year
+DetectHighest("1940")
+print(DetectHighest(1945))
+print(DetectHighest(1950))
+print(DetectHighest(1955))
+print(DetectHighest(1960))
